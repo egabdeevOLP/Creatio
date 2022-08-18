b37d0e816d2275bb27984037133c5bd74c8ea1fb
eldar <eldarg76@gmail.com>
2022-08-18 16:09:17 +0500

    tst 6

--- OmniTest/Schemas/OmniTestService/OmniTestService.cs
+++ OmniTest/Schemas/OmniTestService/OmniTestService.cs
@@ -15,6 +15,7 @@ using Terrasoft.Common;
 
 namespace Terrasoft.Configuration
 {
+    // -------------------------НАЙДИ МЕНЯ------------------------
     public class OmniTestService
     {
         private readonly UserConnection _userConnection;
